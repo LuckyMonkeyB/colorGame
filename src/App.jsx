540dce9f7d6f07c6596745053290acc6b3ac46f3
robinraty <robinraty.dev@gmail.com>
2026-08-28 11:44:52 +0200

    DisplayColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import DisplayColor from './components/DisplayColor'
 
 function App() {
 
   return (
     <>
       <h1>Color Game</h1>
+      <DisplayColor/>
     </>
   )
 }
